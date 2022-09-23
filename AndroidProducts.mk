99359ea5dc3e0f1aaa03c019192602e87a883397
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-23 20:49:49 +0200

    amogus: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: Moto G(8) Family (hOS) Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_amogus.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_amogus-user \
+    omni_amogus-userdebug \
+    omni_amogus-eng
